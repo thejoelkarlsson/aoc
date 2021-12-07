7eb9fc4ac4509cd355187f13ed321c870de660b9
Joel Karlsson <thejoelkarlsson@gmail.com>
2021-12-07 23:02:33 +0100

    Use floor or ceil depending on distance

--- 2021/7/solution.js
+++ 2021/7/solution.js
@@ -3,13 +3,23 @@ const input = fs.readFileSync("input.txt").toString().split(",");
 const inputAsNumbers = input.map((numberAsString) => +numberAsString);
 
 const getMedian = (list) => list.slice().sort((a, b) => a - b)[Math.floor(list.length / 2)];
-const getMean = (list) => Math.floor((list.reduce((prev, curr) => prev + curr) + 1) / list.length);
+const getFloor = (list) => Math.floor(list.reduce((prev, curr) => prev + curr) / list.length);
+const getCeil = (list) => Math.ceil(list.reduce((prev, curr) => prev + curr) / list.length);
 
 getShortest = (list) => list.map((number) => Math.abs(number - getMedian(list))).reduce((prev, curr) => prev + curr);
-getShortestExponential = (list) =>
-  list
-    .map((number) => (Math.abs(number - getMean(list)) * (Math.abs(number - getMean(list)) + 1)) / 2)
-    .reduce((prev, curr) => prev + curr);
+getShortestExponential = (list) => {
+  const floor = getFloor(list);
+  const ceil = getCeil(list);
+  const avg = (floorOrCeil) =>
+    list
+      .map((number) => {
+        const distance = Math.abs(number - floorOrCeil);
+        return (distance * (distance + 1)) / 2;
+      })
+      .reduce((prev, curr) => prev + curr);
+
+  return Math.min(avg(floor), avg(ceil));
+};
 
 const shortestLinear = getShortest(inputAsNumbers);
 console.log(shortestLinear);
